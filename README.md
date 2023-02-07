f4bba21b761fd6a6e18c5c57bd452b8ca306cc7d
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-02-07 14:13:32 +0000

    updated the project using json
    making it a crud application

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Learning React by building a groceries list
 
 still in dev mode
+
+Updated project using json server for CRUD operations
+
+still working on the update section.
+
--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './Header';
 import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
 import AddItem from './AddItem';
+import apiRequest from './apiRequest';
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')))
 
-  const saveItems = (newItems) => {
-    setItems(newItems)
-    localStorage.setItem('shoppingList', JSON.stringify(newItems))
-  }
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    saveItems(listItems)
-  }
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id)
-    saveItems(listItems)
-  }
-
+  const API_URL = 'http://localhost:3500/items'
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [items, setItems] = useState([])
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item }
     console.log(myNewItem)
     const newList = [...items, myNewItem]
-    saveItems(newList)
+    setItems(newList)
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions)
+    if (result) setFetchError(result)
+  }
+
+  const handleCheck = async (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems)
+
+    const myItem = items.filter(item => item.id === id)
+    const optionObj = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: !myItem[0].checked })
+    }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, optionObj)
+    if (result) setFetchError(result)
 
   }
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setItems(listItems)
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!newItem) return;
     addItem(newItem)
     setNewItem('')
   }
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if (!response.ok) throw Error('Did not receive expected data')
+        const listItems = await response.json()
+        setItems(listItems)
+      } catch (err) {
+        setFetchError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+
+    }
+    setTimeout(() => {
+      fetchItems()
+    }, 2000)
+  }, [])
+
 
   return (
     <div className="App">
@@ -51,13 +94,24 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
 
-      <Footer length={items.length} />
+      <main>
+        {isLoading && <p>Loading items....</p>}
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+        }
+      </main>
+
+      {
+        !fetchError && <Footer
+          length={items.length
+          }
+        />
+      }
     </div>
   );
 }
